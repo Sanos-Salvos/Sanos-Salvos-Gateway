f11dba242b8739a56b2074710b278944fab888ce
viyyy <vi.castillop@duocuc.cl>
2026-05-11 20:06:44 -0400

    version 1
    faltan agregar rutas y cambiar puertos

---
+++ gateway/gateway/src/main/java/com/sanosysalvos/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.sanosysalvos.gateway.filter;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+
+    @Value("${jwt.secret:sanosysalvos1234}")
+    private String jwtSecret;
+
+    public JwtAuthenticationFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "Falta la cabecera Authorization", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                return onError(exchange, "Estructura de token inválida", HttpStatus.UNAUTHORIZED);
+            }
+
+            String token = authHeader.substring(7);
+
+            try {
+                SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+                Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+            } catch (Exception e) {
+                return onError(exchange, "Token JWT inválido o expirado", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
+    }
+}
