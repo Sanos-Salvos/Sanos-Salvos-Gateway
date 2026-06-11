3656686c4ee240129f7210f2113d417e6b7c9fc2
NicoDuoc02 <169114151+NicoDuoc02@users.noreply.github.com>
2026-06-10 20:41:35 -0400

    fix: usar init-db.sql en vez de init-db.sh para evitar problemas de permisos en Windows

---
+++ init-db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE mascotas_db;
+CREATE DATABASE usuarios_db;
+CREATE DATABASE organizaciones_db;
+CREATE DATABASE coincidencias_db;
+CREATE DATABASE geolocalizacion_db;
+CREATE DATABASE notificaciones_db;
